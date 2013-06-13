7d709876f470e6f6695eb904306dda156d261935
Alessio Mansutti <mansutti.alessio@spes.uniud.it>
2013-06-13 13:16:33 +0000

    Lexer/Parser e Pretty Printer per BigMC. Note: Date le modifiche alla libreria, le classi in jlibbig.udlang non sono più funzionanti. Manca implementazione AbstBigraph da parte di RedexBigraph (metodi non ancora usati) Manca accorgimento per poter scrivere regole non esprimibili come bigrafi, ma usabili in BigMC.

---
+++ it.uniud.bigraphs/src/jlibbig/bigmc/BigraphSystem.java
@@ -0,0 +1,53 @@
+package jlibbig.bigmc;
+
+import jlibbig.core.*;
+import java.util.*;
+
+
+public class BigraphSystem{
+	private Signature signature;
+	private Set<Bigraph> bigraphs;
+	private Set<String> outerNames;
+	private Map<RedexBigraph , RedexBigraph> reactionRules;
+			
+	public BigraphSystem(){
+		signature = null;
+		bigraphs = new HashSet<>();
+		outerNames = new HashSet<>();
+		reactionRules = new HashMap<>();
+	}
+			
+	public void setSignature( Signature sig ){ 
+		signature = sig; 
+	}
+	
+	public void addName( String name ){
+		outerNames.add( name );
+	}
+			
+	public Signature getSignature(){ 
+		return signature; 
+	}
+
+	public Set<String> getOuterNames(){
+		return Collections.unmodifiableSet( outerNames );			
+	}
+
+	public void addBigraph( Bigraph b ){
+		bigraphs.add( b );
+	}
+			
+	public void addReaction( Bigraph redex , Bigraph reactum ){
+		if( redex.getRoots().size() != reactum.getRoots().size() )
+			throw new RuntimeException("The number of roots in redex and reactum must be the same");
+		reactionRules.put( new RedexBigraph( redex , outerNames ) , new RedexBigraph( reactum , outerNames ) );
+	}
+			
+	public Set<Bigraph> getBigraphs(){
+		return Collections.unmodifiableSet( bigraphs );
+	}
+			
+	public Map<RedexBigraph , RedexBigraph> getReactions(){
+		return Collections.unmodifiableMap( reactionRules );
+	}
+}
---
+++ it.uniud.bigraphs/src/jlibbig/bigmc/lang/BigMCPrinter.java
@@ -0,0 +1,98 @@
+package jlibbig.bigmc.lang;
+
+import java.util.*;
+
+import jlibbig.bigmc.*;
+import jlibbig.core.*;
+
+public class BigMCPrinter{
+	private BigMCPrinter(){}
+	
+	public static String toString( BigraphSystem brs ){
+		String ln = System.getProperty("line.separator");
+		StringBuilder s = new StringBuilder();
+		
+		for( Control ctrl : brs.getSignature() ){
+				s.append( (ctrl.isActive() ? "%active " : "%passive ")
+						+ ctrl.getName() + " : " + ctrl.getArity() +";" + ln );
+		}
+		
+		s.append( ln );
+		
+		for( String str : brs.getOuterNames() )
+			s.append( "%name " + str + ";" + ln );
+		
+		s.append( ln );
+		
+		for(Map.Entry<RedexBigraph, RedexBigraph> reaction : brs.getReactions().entrySet() )
+			s.append( toString( reaction.getKey().getBigraph() ) + " -> " + toString( reaction.getValue().getBigraph() ) + " ;" + ln );
+		
+		s.append( ln );
+		
+		for( Bigraph big : brs.getBigraphs() )
+			s.append( toString( big ) + " ;" + ln );
+		
+		s.append( ln );
+		
+		return s.toString();
+	}
+	
+	public static String toString( Bigraph big ){
+		StringBuilder s = new StringBuilder();
+		
+		Iterator<? extends Root> it = big.getRoots().iterator();
+		while( it.hasNext() ){
+			
+			Set<? extends Child> childs = it.next().getChildren();
+			if( !childs.isEmpty() ){
+				Iterator<? extends Child> childIt = childs.iterator();
+				while( childIt.hasNext() )
+					s.append( toString( childIt.next() , big.getOuterNames() , big.getSites() ) + (childIt.hasNext() ? " | " : "") );
+			}else s.append( "nil" );
+			s.append( it.hasNext() ? " || " : "" );
+		}
+		return s.toString();
+	}
+	
+	private static String toString( Child c , Set<? extends OuterName> outerNames , List<? extends Site> sites ){
+		StringBuilder s = new StringBuilder();
+		
+		if( c instanceof Site ){
+			s.append( "$" + sites.indexOf(c) );
+		}else{
+			s.append( ((Node) c).getControl().getName() );
+			
+			StringBuilder ns = new StringBuilder();
+			int unlinked = 0;
+			
+			Iterator<? extends Port> portIt = ((Node) c).getPorts().iterator();
+			while( portIt.hasNext() ){
+				Handle handle = portIt.next().getHandle();
+				if( outerNames.contains( handle ) ){
+					for( int i = 0; i < unlinked; ++i )
+						ns.append( " - , " );
+					ns.append( " " + ((OuterName) handle ).getName() + " , " );
+				}else
+					++unlinked;
+			}
+			
+			if(ns.length() > 0 )	//note: if length > 0 --> at least 1 append has been done --> length > 3
+				s.append( "[" + ns.substring(0 , ns.length()-2 ) + "]" );
+			
+			Set<? extends Child> childs = ((Node) c ).getChildren();
+			if(! childs.isEmpty() ){
+				s.append(".");
+				if( childs.size() > 1 ) s.append("( ");
+				
+				Iterator<? extends Child> childIt = childs.iterator();
+				while( childIt.hasNext() )
+					s.append( toString( childIt.next() , outerNames , sites ) + (childIt.hasNext() ? " | " : "") );
+				
+				if( childs.size() > 1 ) s.append(" )");
+			}
+		}
+		
+		return s.toString();
+	}
+	
+}
