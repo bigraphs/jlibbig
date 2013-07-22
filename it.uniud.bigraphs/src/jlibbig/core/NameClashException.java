678a5d2014d2bced6a46d740ee78783a90aa15a0
Marco Peressotti <mperessotti@users.noreply.github.com>
2013-07-22 16:53:53 +0000

    Eccezioni

--- it.uniud.bigraphs/src/jlibbig/core/NameClashException.java
+++ it.uniud.bigraphs/src/jlibbig/core/NameClashException.java
@@ -1,31 +1,53 @@
 package jlibbig.core;
 
+import java.util.*;
+
 public class NameClashException extends RuntimeException {
 	private static final long serialVersionUID = 7906403825825798881L;
+
+	private final Set<String> names;
+
+	public NameClashException(Set<String> names){
+		this(null, names);
+	} 
 	
-	private final String name;
-	
-	public NameClashException(String message){
-		this(message,null);
-	}
-		
-	public NameClashException(String message, String name){
+	public NameClashException(String message, Set<String> names) {
 		super(message);
-		this.name = name;
+		if (!names.isEmpty()) {
+			Set<String> ns = new HashSet<>();
+			for (String n : names) {
+				ns.add(n);
+			}
+			this.names = Collections.unmodifiableSet(ns);
+		} else {
+			this.names = null;
+		}
 	}
-		
-	public String getName(){
-		return this.name;
+	
+	public NameClashException(String message, String... names) {
+		super(message);
+		if (names.length > 0) {
+			Set<String> ns = new HashSet<>();
+			for (int i = 0; i < names.length; i++) {
+				ns.add(names[i]);
+			}
+			this.names = Collections.unmodifiableSet(ns);
+		} else {
+			this.names = null;
+		}
+	}
+
+	public Set<String> getNames() {
+		return this.names;
 	}
 
 	@Override
 	public String toString() {
-		if(name != null){
-			return super.toString() + "\nClash over '" + name + "'";
-		}else{
+		if (names != null) {
+			return super.toString() + "\nClash over '" + names + "'";
+		} else {
 			return super.toString();
 		}
 	}
-	
-	
+
 }
