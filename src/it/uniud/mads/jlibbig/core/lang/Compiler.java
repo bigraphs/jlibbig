522f886fd0dffe3e8c1b23dad48ec71079080fbd
Marco Peressotti <mperessotti@users.noreply.github.com>
2015-09-02 11:51:21 +0200

    Reorg: - rimossi src/tests - BigMC e UDLang negli appositi branch

--- src/it/uniud/mads/jlibbig/core/lang/Compiler.java
+++
@@ -1,9 +0,0 @@
-package it.uniud.mads.jlibbig.core.lang;
-
-import java.io.*;
-
-public interface Compiler<T> {
-	T parse(String string) throws CompilerException;
-
-	T parse(Reader in) throws CompilerException;
-}
