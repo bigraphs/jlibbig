a403688671260bf3536b6b6a54c3b424a91415fd
Marco Peressotti <mperessotti@users.noreply.github.com>
2015-09-08 14:45:13 +0200

    Variable block size

--- src/it/uniud/mads/jlibbig/core/util/NameGenerator.java
+++ src/it/uniud/mads/jlibbig/core/util/NameGenerator.java
@@ -7,16 +7,19 @@ import java.math.BigInteger;
 
 public class NameGenerator {
 
-	private final static boolean DEBUG_NAME_GEN = Boolean.getBoolean("it.uniud.mads.jlibbig.namegeneration");
+	private final static boolean DEBUG_NAME_GEN = true; //Boolean.getBoolean("it.uniud.mads.jlibbig.namegeneration");
 	
 	public static final NameGenerator DEFAULT = new NameGenerator();
+
+	protected static final long MIN_BLOCK_SIZE = 1000L;
+	protected static final long MAX_BLOCK_SIZE = 1000000L;
 	
 	private BigInteger _sharedCounter = BigInteger.ZERO; 
 	
 	private final ThreadLocal<Block> _localBlock = new ThreadLocal<Block>(){
 		@Override
 		protected Block initialValue(){
-			return new Block();
+			return createLocalBlock();
 		}
 	};
 	
@@ -38,27 +41,61 @@ public class NameGenerator {
 		return block;
 	}
 	
-	private class Block{
+	protected Block createLocalBlock(){
+		return new Block();
+	}
+	
+	protected class Block{
+		
+		private static final long SHRINK_FACTOR = 2;
+		private static final long GROW_FACTOR = 2;
+		
 		private long _rem = 0;
-		private long _blockSizeL = 2000;
-		private BigInteger _blockSize = BigInteger.valueOf(_blockSizeL);
-//		private long _lastGen = 0;
+		private long _currentSize = MIN_BLOCK_SIZE*2;
+		private BigInteger _blockSize = BigInteger.valueOf(_currentSize);
+		private long _lastGenT = 0;
+		private int _genSinceLastSizeCng = -1;
 		private BigInteger _next = null;
 		
 		Block(){}
+		
+		public long getSize(){
+			return this._currentSize;
+		}
+		
+		protected long onRefilling(long oldBlockSize){
+			long now = System.currentTimeMillis();
+			long newBlockSize = oldBlockSize;
+			if(now - _lastGenT < 1000 && newBlockSize * GROW_FACTOR <= MAX_BLOCK_SIZE){
+				newBlockSize *= GROW_FACTOR;
+			}else if(_genSinceLastSizeCng > 5 && newBlockSize / SHRINK_FACTOR >= MIN_BLOCK_SIZE){
+				newBlockSize /= SHRINK_FACTOR;
+			}
+			return newBlockSize;
+		}
 				
+		private void refill(){
+			long newBlockSize = onRefilling(this._currentSize);
+			if(newBlockSize != _currentSize && newBlockSize >= MIN_BLOCK_SIZE){// && newBlockSize <= MAX_BLOCK_SIZE ){
+				_currentSize = newBlockSize;
+				_genSinceLastSizeCng = 0;
+				_blockSize = BigInteger.valueOf(_currentSize);
+			}else{
+				_lastGenT = System.currentTimeMillis();
+				_genSinceLastSizeCng += 1;
+			}
+			_next = getNewBlock(_blockSize);
+			_rem = _currentSize;
+		}
+		
 		public String next(){
 			if(_rem < 1){
-//				long n = System.currentTimeMillis();
-				_next = getNewBlock(_blockSize);
-				_rem = _blockSizeL;
-//				_lastGen = n;
+				refill();
 			}
 			_rem -= 1;
 			String name = this._next.toString(16).toUpperCase();
 			this._next = this._next.add(BigInteger.ONE);
 			return name;
 		}
-		
 	}
 }
