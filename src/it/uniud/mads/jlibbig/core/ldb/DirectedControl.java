42c6a4b03a6dd8d648def5fafbade11ad69b2c32
Fabio Burco <burco.fabio@spes.uniud.it>
2016-07-23 12:34:55 +0200

    Work on DirectedControls

---
+++ src/it/uniud/mads/jlibbig/core/ldb/DirectedControl.java
@@ -0,0 +1,181 @@
+package it.uniud.mads.jlibbig.core.ldb;
+
+import it.uniud.mads.jlibbig.core.attachedProperties.Property;
+import it.uniud.mads.jlibbig.core.attachedProperties.PropertyContainer;
+import it.uniud.mads.jlibbig.core.attachedProperties.PropertyTarget;
+import it.uniud.mads.jlibbig.core.std.Bigraph;
+import it.uniud.mads.jlibbig.core.std.Node;
+import it.uniud.mads.jlibbig.core.std.Signature;
+import it.uniud.mads.jlibbig.core.util.NameGenerator;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * Objects created from this class are bigraphical controls describing the
+ * modality (i.e. active or passive) and the arity (i.e. the number of ports) of
+ * nodes decorated with it. Every {@link Bigraph} has a {@link Signature}
+ * describing the controls that can be assigned to its nodes; every {@link Node}
+ * should be assigned exactly one control.
+ */
+public final class DirectedControl extends it.uniud.mads.jlibbig.core.Control implements PropertyTarget {
+
+    protected int arityPlus;
+    protected int arityMinus;
+
+    private final boolean active;
+
+    private final PropertyContainer props;
+
+    /**
+     * Creates a control for the given arity and modality and assign it a fresh
+     * name.
+     *
+     * @param active     a boolean indicating whether the control is active or passive.
+     * @param arityPlus  a non-negative integer defining the number of upper ports of the
+     *                   nodes decorated with this control.
+     * @param arityMinus a non-negative integer defining the number of lower ports of the
+     *                   nodes decorated with this control.
+     */
+    public DirectedControl(boolean active, int arityPlus, int arityMinus) {
+        this("DC_" + NameGenerator.DEFAULT.generate(), active, arityPlus, arityMinus);
+    }
+
+    /**
+     * Creates a control for the given name, arity and modality.
+     *
+     * @param name       the name of the control.
+     * @param active     a boolean indicating whether the control is active or passive.
+     * @param arityPlus  a non-negative integer defining the number of upper ports of the
+     *                   nodes decorated with this control.
+     * @param arityMinus a non-negative integer defining the number of lower ports of the
+     *                   nodes decorated with this control.
+     */
+    public DirectedControl(String name, boolean active, int arityPlus, int arityMinus) {
+        super(name, arityPlus + arityMinus);
+        this.arityPlus = arityPlus;
+        this.arityMinus = arityMinus;
+        this.active = active;
+        this.props = new PropertyContainer(this);
+    }
+
+    /**
+     * Get control's positive arity. This corresponds to the number of upper ports of a node.
+     *
+     * @return control's positive arity.
+     */
+    public int getArityPlus() {
+        return arityPlus;
+    }
+
+    /**
+     * Get control's negative arity. This corresponds to the number of lower ports of a node.
+     *
+     * @return control's negative arity.
+     */
+    public int getArityMinus() {
+        return arityMinus;
+    }
+
+    /**
+     * Checks if the control is active.
+     *
+     * @return a boolean indicating whether the control is active or passive.
+     */
+    public final boolean isActive() {
+        return active;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + getArity();
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        DirectedControl other = (DirectedControl) obj;
+        return !(getArityPlus() != other.getArityPlus()
+                || getArityMinus() != other.getArityMinus()
+                || !super.getName().equals(other.getName()));
+    }
+
+    String _tostring;
+
+    @Override
+    public String toString() {
+        return this.toString(false);
+    }
+
+    public String toString(boolean includeProperties) {
+        StringBuilder builder = null;
+        if (_tostring == null) {
+            builder = new StringBuilder();
+            builder.append(getName()).append(":(").append(getArityPlus()).append(",").append(getArityMinus()).append((active) ? ",a)" : ",p)");
+            _tostring = builder.toString();
+        }
+        if (includeProperties) {
+            Collection<Property<?>> ps = props.getProperties();
+            if (ps.isEmpty())
+                return _tostring;
+            if (builder == null) {
+                builder = new StringBuilder(_tostring);
+            }
+            builder.append(" [");
+            Iterator<Property<?>> ip = ps.iterator();
+            while (ip.hasNext()) {
+                Property<?> p = ip.next();
+                builder.append(p.getName()).append('=').append(p.get());
+                if (ip.hasNext()) {
+                    builder.append("; ");
+                } else {
+                    builder.append(']');
+                }
+            }
+            return builder.toString();
+        } else {
+            return _tostring;
+        }
+    }
+
+	/* Properties handling */
+
+    @Override
+    public Property<?> attachProperty(Property<?> prop) {
+        return props.attachProperty(prop);
+    }
+
+    @Override
+    public <V> Property<V> detachProperty(Property<V> prop) {
+        return props.detachProperty(prop);
+    }
+
+    @Override
+    public <V> Property<V> detachProperty(String name) {
+        return props.detachProperty(name);
+    }
+
+    @Override
+    public <V> Property<V> getProperty(String name) {
+        return props.getProperty(name);
+    }
+
+    @Override
+    public Collection<Property<?>> getProperties() {
+        return props.getProperties();
+    }
+
+    @Override
+    public Collection<String> getPropertyNames() {
+        return props.getPropertyNames();
+    }
+
+}
